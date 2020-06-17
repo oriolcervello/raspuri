03a3fe3e53d7b5eecd273059582f494b78663529
oriolcervello <42965849+oriolcervello@users.noreply.github.com>
2020-06-17 20:29:14 +0200

    Update ftp_call.py

--- raspuri2_build/RASPURI/region/ftp_call.py
+++ raspuri2_build/RASPURI/region/ftp_call.py
@@ -1,7 +1,6 @@
 #==========================================================================
-#   RASPURI v2 a RASP(Regional Atmospheric
-#   Soaring Predictions) COMPUTER PROGRAM
-#   Original Creator: Oriol Cervelló i Nogués ( raspuri [at] protonmail.com )
+#   RASPURI v2. github.com/oriolcervello/raspuri
+#   Oriol Cervelló i Nogués, ( raspuri [at] protonmail.com ).
 #   Copyright (C) 2019+ by Oriol Cervelló i Nogués
 #==========================================================================
 # License: GNU GPLv3.0
