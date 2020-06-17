557a9f755eb844a273055fa9ef877c89cba3cd49
oriolcervello <42965849+oriolcervello@users.noreply.github.com>
2020-06-17 20:57:18 +0200

    Update plot.py

--- raspuri2_build/RASPURI/region/plot.py
+++ raspuri2_build/RASPURI/region/plot.py
@@ -176,6 +176,11 @@ def plot_crop(filename):
     im.crop((1096, 26, 1385, 761)).save(filename+'L.png')
     im.crop((176, 97, 1043, 712)).save(filename+'.png')
 
+def plot_crop_stream(filename):
+    im = Image.open(filename+'.png')
+    #w, h = im.size
+    im.crop((1096*2, 26*2, 1385*2, 761*2)).save(filename+'L.png')
+    im.crop((176*2, 97*2, 1043*2, 712*2)).save(filename+'.png')
 
 def plot_cont_layer(prop_f,prop,ptitle,filename,bounds,ls,ve,mosaic,x,y,valid_t,dx,dy,cmap,vmin,vmax,units):
     try:
@@ -227,8 +232,9 @@ def plot_strem_layer(U,V,prop,ptitle,filename,bounds,ls,ve,mosaic,x,y,valid_t,dx
 
         ##SAVE    
         plt.savefig(filename+'.png', transparent=True)
+        plt.savefig(filename+'original.png', transparent=True)
         plt.close()
-        plot_crop(filename)
+        plot_cont_layer(filename)
     except Exception as e: 
         print('****Error ploting the '+ptitle+', plot.py****')
         print(e)
